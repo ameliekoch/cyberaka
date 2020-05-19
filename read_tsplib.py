3f0ab4dd142142dfcff0c46055bf8643762eda86
JosuaKugler <josua.kugler@gmail.com>
2020-05-19 23:34:09 +0200

    removed unnecessary files

--- read_tsplib.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from pprint import pprint
-import tsplib95
-import networkx as nx
-from networkx.drawing.nx_pydot import write_dot
-problem = tsplib95.load("tsplib/gr17.tsp")
-G = problem.get_graph()
-
-print(G.edges[0,2])
